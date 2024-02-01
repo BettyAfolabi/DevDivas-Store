d2133708b28d4e24c3b60404cddce3ac3d667f66
faithadewuyi <iyanuleke09@gmail.com>
2024-02-01 13:08:04 +0100

    Chore: toggle button responsive  on small screen and styling adjusted

--- script2.js
+++ script2.js
@@ -1,34 +1,54 @@
-const togglebutton = document.getElementById("toggleMenuBar")
-const menuBar = document.querySelector(".menuBar")
+// This script handles the toggle button and fetch Category
+
+const toggleButton = document.getElementById("toggleMenuBar");
+const menuBar = document.querySelector(".menuBar");
+const categoriesContainer = document.getElementById('categories');
+ 
+// Function to toggle the display of the categories
+
+categoriesContainer.style.center = "250px";
+toggleButton.onclick = function(){
+    categoriesContainer.style.right =(categoriesContainer.style.right === "-250px") ? "0" : "-250px";
+    toggleButton.textContent = (categoriesContainer.style.right === "-250px") ? '✕' : '☰';
+};
 
 
+ function toggleCategoriesDisplay() {
+    if (categoriesContainer.style.display === 'block') {
+       categoriesContainer.style.display = 'none';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const categoriesContainer = document.getElementById('categories')
+  } else {
+      categoriesContainer.style.display = 'block';
+
+  }
+ }
+ 
+// Event listener for the toggle button
+ toggleButton.addEventListener('click', toggleCategoriesDisplay);
+ 
+document.addEventListener('DOMContentLoaded',  () => {
+    
+
+    // Fetch categories and display them
+    async function fetchCategory() {
+        const res = await fetch('https://fakestoreapi.com/products/categories');
+        const category = await res.json();
+        console.log(category)
+        return category;
+    }
+ 
+    function displayCategories(categories) {
+        categories.forEach(category => {
+            const catDiv = document.createElement('div');
+            catDiv.className = 'category';
+            catDiv.innerText = category;
+            categoriesContainer.appendChild(catDiv);
+        });
+        
+    }
 
     
-    async function fetchCategory(){
-        const res = await fetch('https://fakestoreapi.com/products/categories')
-        const category = await res.json()
-        return category
-    }
-
-    function displayCategories(categories){
-        categories.forEach(category => {
-            const catDiv = document.createElement('div')
-            catDiv.className = 'category'
-            catDiv.innerText = category
-            categoriesContainer.appendChild(catDiv)
-        })
-    }
-
-    fetchCategory().then(displayCategories)
-})
-
-// togglebutton.addEventListener("click", function(){
-//     menuBar.classList.toggle("block")
-// })
-togglebutton.onclick = function(){
-    menuBar.classList.toggle("hidden")
-    displayCategories(categories)
-}
+ 
+    fetchCategory().then(displayCategories);
+});
+ 
